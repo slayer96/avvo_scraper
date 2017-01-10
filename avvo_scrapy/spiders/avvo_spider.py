93f7926ec6b7d3f238eed3a499127005b159b3b0
slayer96 <myhailo.mykytyn.96@gmail.com>
2017-01-10 22:54:59 +0200

    change check_account_rating

--- avvo_scrapy/spiders/avvo_spider.py
+++ avvo_scrapy/spiders/avvo_spider.py
@@ -1,4 +1,7 @@
+import logging
+
 import scrapy
+from scrapy.utils.log import configure_logging
 from scrapy.spiders import CrawlSpider
 
 from ..items import AvvoScrapyItem
@@ -7,7 +10,16 @@ STARS = 4.0
 URL = 'https://www.avvo.com'
 
 
+configure_logging(install_root_handler=False)
+logging.basicConfig(
+    filename='log.txt',
+    format='%(levelname)s: %(message)s',
+    level=logging.INFO
+)
+
+
 class AvvoSpider(CrawlSpider):
+
     name = 'avvospider'
     start_urls = [
         'https://www.avvo.com/search/lawyer_search.html?page='
@@ -20,36 +32,41 @@ class AvvoSpider(CrawlSpider):
             pagination_page_numbers = pagination_page_numbers[0] + 1
         else:
             pagination_page_numbers = pagination_page_numbers[0]
-
+        pagination_page_numbers = 1
         for page_number in range(1, pagination_page_numbers+1):
+            logging.info(page_number)
             url = ''.join([self.start_urls[0], str(page_number)])
             yield scrapy.Request(url, callback=self.get_page_urls)
 
     def get_page_urls(self, response):
-        page_urls = response.xpath('//div/a[@itemprop="url"]/@href').extract()
+        page_urls = check_account_rating(response)
+        logging.info(page_urls)
         for page_url in page_urls:
+            logging.info(page_url)
             url = ''.join([URL, page_url])
             yield scrapy.Request(url, callback=self.parse_account_page)
 
     def parse_account_page(self, response):
         item = AvvoScrapyItem()
-        account_rating = check_account_rating(response)
-        if account_rating:
-            item['url'] = response.url
-            item['name'] = response.xpath('//span[@itemprop="name"]/text()').extract_first()
-            item['address'] = ''.join(response.xpath('//span[@itemprop="address"]/p/span/text()').extract())
-            item['phone'] = response.xpath('//span[@itemprop="telephone"]/a/span/text()').extract_first()
-            item['website'] = response.xpath('//p[@itemprop="url"]/@content').extract_first()
-            yield item
-        else:
-            yield
+        item['url'] = response.url
+        item['name'] = response.xpath('//span[@itemprop="name"]/text()').extract_first()
+        item['address'] = ''.join(response.xpath('//span[@itemprop="address"]/p/span/text()').extract())
+        item['phone'] = response.xpath('//span[@itemprop="telephone"]/a/span/text()').extract_first()
+        item['website'] = response.xpath('//p[@itemprop="url"]/@content').extract_first()
+        yield item
 
 
 def check_account_rating(response):
-    rating_value = response.xpath('//span[@itemprop="ratingValue"]/@content').extract_first()
-    if rating_value and float(rating_value) <= STARS:
-        return True
-    else:
-        return False
+    page_urls = []
+    account_xpath_list = response.xpath('//div[@itemscope="itemscope"]')
+    for account in account_xpath_list:
+        rating = account.xpath('div//span[@class="sr-only"]/text()').extract_first()
+        if not rating.isalpha() and float(rating[:3]) <= STARS:
+
+            page_urls.append(account.xpath('div//a/@href').extract_first())
+    return page_urls
+
+
+
 
 
